f6bc7f8f464a6f0892aaa26baba79cacd36e35f6
michael <smv.bild@gmail.com>
2020-04-02 19:43:03 +0200

    Added menu and maze setup

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pygame
+import sys
 import random
 
 pygame.init()
 
-WIDTH = 400
-HEIGHT = 400
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
+
+
+WIDTH = 600
+HEIGHT = int(WIDTH * 2/3)
 
 clock = pygame.time.Clock()
 
@@ -37,7 +41,13 @@ class Rect(object):
     def draw(self):
         pygame.draw.rect(screen, self.typedict[self.type]['c'], pygame.Rect(self.x,self.y,self.w,self.h))
 
-    def is_clicked(self):
+    def update(self, started):
+        if started:
+            pass
+        else:
+            self.is_clicked_prep()
+
+    def is_clicked_prep(self):
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
             if (pos[0] >= self.x and pos[0] <= self.x + self.w 
@@ -46,9 +56,6 @@ class Rect(object):
                 index = typedict_keys.index(self.type) 
                 index = (index + 1) % len(typedict_keys)
                 self.type = typedict_keys[index]
-                return True
-            else:
-                return False
 
         if pygame.mouse.get_pressed()[2]:
             pos = pygame.mouse.get_pos()
@@ -61,32 +68,30 @@ class Rect(object):
                     index = len(typedict_keys) - 1
 
                 self.type = typedict_keys[index]
-                return True
-            else:
-                return False
 
 class Grid(object):
-    def __init__(self, width, height, padding):
+    def __init__(self, width, height, padding=2):
         self.width = int(width)
         self.height = int(height)
+        self.end_width = 2/3 * WIDTH
+        self.started = False
 
         if padding < 1:
-            print('Grid __init__: padding too small. Setting padding to 1')
-            self.padding = 1
+            print('Grid __init__: padding too small. Setting padding to 2')
+            self.padding = 2
         else:
             self.padding = padding
+    
+        
+        self.effective_width = self.end_width - (self.width + 1) * padding
+        self.effective_height = HEIGHT - (self.height + 1) * padding
 
-        effective_width = WIDTH - (self.width + 1) * padding
-        effective_height = HEIGHT - (self.height + 1) * padding
+        self.effective_width_rect = int(self.effective_width / self.width)
+        self.effective_height_rect = int(self.effective_height / self.height)
 
-        effective_width_rect = int(effective_width / self.width)
-        effective_height_rect = int(effective_height / self.height)
+        self.leftover_space_vertical = HEIGHT - (self.height * self.effective_height_rect + padding * (self.height + 1))
 
-        leftover_space_horizontal = WIDTH - (self.width * effective_width_rect + padding * (self.width + 1))
-        leftover_space_vertical = HEIGHT - (self.height * effective_height_rect + padding * (self.height + 1))
-
-        start_x = (leftover_space_horizontal + padding) // 2
-        start_y = (leftover_space_vertical + padding) // 2
+        self.start_y = (self.leftover_space_vertical + padding) // 2
 
         self.rects = []
 
@@ -96,10 +101,10 @@ class Grid(object):
         for i in range(self.width):
             for j in range(self.height):
 
-                x = i * effective_width_rect + padding * i + start_x
-                y = j * effective_height_rect + padding * j + start_y
-                w = effective_width_rect
-                h = effective_height_rect
+                x = i * self.effective_width_rect + self.padding * i + 1
+                y = j * self.effective_height_rect + self.padding * j + self.start_y
+                w = self.effective_width_rect
+                h = self.effective_height_rect
 
                 self.rects[i].append(Rect(x, y, w, h))
         
@@ -111,31 +116,123 @@ class Grid(object):
     def update(self):
         for i in range(self.width):
             for j in range(self.height):
-                if(self.rects[i][j].is_clicked()):
-                    print(i, j)
+                self.rects[i][j].update(self.started)
 
+    def clear(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                self.rects[i][j].type = 'empty'
+
+class Button(object):
+    def __init__(self, x, y, w, h, function, grid):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.function = function
+
+        def start():
+            if not grid.started:
+                print('Started the pathfinding process')
+                grid.started = True
+
+        def quit():
+            pygame.quit()
+            sys.exit()
+
+        def clear():
+            grid.clear()
+            grid.started = False
+
+        self.funcdict = {
+                'start': start,
+                'quit': quit,
+                'clear': clear
+                }
+
+    def draw(self):
+        pygame.draw.rect(screen, (255,255,255), (self.x, self.y, self.w, self.h))
+        textsurface = myfont.render(f'{self.function.upper()}', False, (0,0,0))
+        screen.blit(textsurface, (self.x - 25 + self.w // 2, self.y + self.h // 2))
+
+    def is_clicked(self):
+        if pygame.mouse.get_pressed()[0]:
+            pos = pygame.mouse.get_pos()
+            if (pos[0] >= self.x and pos[0] <= self.x + self.w 
+                and pos[1] >= self.y and pos[1] <= self.y + self.h):
+                self.funcdict[self.function]()
+
+class Menu(object):
+    def __init__(self, x, grid, padding=2):
+        if padding < 1:
+            print('Menu __init__: padding too small. Setting padding to 2')
+            self.padding = 2
+        else:
+            self.padding = padding
+
+        self.x = x + self.padding
+        self.y = self.padding
+        self.width = WIDTH - self.x - self.padding
+        self.height = HEIGHT - self.y - self.padding
+        
+        self.funcdict = {
+                0: 'start',
+                1: 'clear',
+                2: 'quit'
+                }
+
+        self.num_buttons = 3
+        self.button_height = (self.height - 2 * padding) // self.num_buttons
+
+        print(self.button_height)
+
+        self.buttons = []
+
+        for i in range(self.num_buttons):
+            self.buttons.append(Button(self.x, i * self.button_height + self.y + i * self.padding, self.width, self.button_height, self.funcdict[i], grid))
+
+        print(self.buttons)
+
+    def draw(self):
+        for button in self.buttons:
+            button.draw()
+
+    def update(self):
+        for button in self.buttons:
+            button.is_clicked()
+
+
+grid = Grid(7, 7)
+menu = Menu(int(grid.end_width), grid, 2)
+
+def draw():
+    grid.draw()
+    menu.draw()
+
+def update():
+    grid.update()
+    menu.update()
+        
         
 
 def main():
-    grid = Grid(7, 7, 2)
 
-    done = False
-
-    while not done:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 done = True
 
         screen.fill((51,51,51))
 
-        grid.update()
-        grid.draw()
+        draw()
+        update()
 
         pygame.display.flip()
 
         clock.tick(8)
 
     pygame.quit()
+    sys.exit()
 
 if __name__ == '__main__':
     main()
